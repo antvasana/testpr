95afdd94c63792643c1ed9c22064187b186ab9be
Tatiana <tgurieva04@gmail.com>
2023-12-11 21:22:16 -0400

    first

---
+++ js/script.js
@@ -0,0 +1,146 @@
+"use strict";
+
+// console.log(10);
+// alert(20);
+
+// let age = 29;
+// age = 30;
+// console.log(age);
+
+// let age = 29, user = "Andrey", message = 'Hello';
+// console.log(age);
+// console.log(user);
+// console.log(message);
+
+// console.log(1 / 0);
+// console.log(Infinity);
+// console.log('text' / 2);  // Nan
+
+// let str = "Привет";
+// let str1 = 'Hello';
+// let str2 = `Hi`;
+// console.log(str);
+// console.log(str);
+// console.log(str1);
+// console.log(str1);
+// console.log(str2);
+
+// let str2 = `Hi - ${str}`;
+// console.log(str2);
+// let str3 = 'HI' + '' + '-' + '' + str;
+// console.log(str3);
+// let str4 = `Hi - ${1+2}`;
+// console.log(str4);
+
+// let nameChecked = true;
+// let ageChecked = false;
+// console.log(nameChecked);
+// console.log(ageChecked);
+// console.log(2 < 1);
+
+// let age;
+// console.log(age);
+
+// typeof undefined;
+
+// alert('hello');
+
+// let result = prompt('Text');
+// let age = prompt('Сколько тебе лет?', 28);
+// alert(`Тебе ${age} лет!`);
+
+// let result = confirm("question");
+
+// let result = confirm('Тебя зовут Андрей?');
+// alert(result);
+
+// console.log(2 * 3);
+// let i = 2;
+// i = -i;
+// console.log(i);
+
+// let x = 2, y = 3;
+// console.log(y - x);
+
+// console.log(5 % 2);
+// console.log(2 ** 3);
+
+// console.log(4 ** (1 / 2));
+
+// let text = 3 * "2";
+// console.log(text);  //6
+
+// let number = 3 + "4";
+// console.log(number);
+
+// let x = true;
+// console.log(+x);  // 1
+
+// let x = "1";
+// let y = "2";
+// console.log(x + y);  // 12
+// console.log(+x + +y);  // 3
+
+// let counter = 4;
+// counter++;
+// console.log(counter);  //5
+
+// let counter2 = 7;
+// ++counter2;
+// console.log(counter2);  //8
+
+// let counter = 2;
+// let a = counter++;
+// console.log(a);  //2
+
+// let counter2 = 2;
+// let b = ++counter2;
+// console.log(b);  //3
+
+// let counter = 2;
+// // console.log(2 * ++counter);  //6
+// console.log(2 * counter++);  //4
+// console.log(2* counter++);  //6
+
+// let a = (1 + 2, 3 + 4);
+// console.log(a);  //7
+
+// console.log(true || true);
+// console.log(false || true);
+// console.log(true || false);
+// console.log(false || false);
+
+// let currentUser = null;
+// let defaultUser = "Andrey";
+// let name = currentUser || defaultUser || "unamed";
+// console.log(name);  //Andrey
+
+// let x;
+// true || (x = 1);
+// console.log(x);  //undefined
+
+// let x;
+// true || (x = 1);
+// console.log(x);  //1
+
+// console.log(true && true);
+// console.log(false && true);
+// console.log(true && false);
+// console.log(false && false);
+
+// let hour = 12;
+// let minute = 30;
+// if (hour == 12 && minute == 30) {
+//   console.log('Сейчас 12:30');
+// }
+
+
+// console.log(!true); //false
+
+console.log(!!null); //false
+console.log(!!"Text");  //true
+ 
+
+
+
+
